b6ad46ba46bd396455ef3660d61a412cb269eea5
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-07-01 23:41:31 -0300

    initial commit 🚀

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.join(__dirname, 'src') }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
+  },
+})
