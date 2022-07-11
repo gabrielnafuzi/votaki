fa79adfe98384f02fd4834e6705f6e9d3eebcd19
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-07-11 16:43:14 -0300

    add chromatic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { Input } from '@/components/input'
-
 export default function HomePage() {
   return (
     <div>
       <h1>Hello world</h1>
-
-      <Input label="hello" name="input" />
     </div>
   )
 }
