3dd167fff60a671d91def40f87ca4e2ba45a88bb
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-07-29 01:19:46 -0300

    fix next auth file name

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -3,7 +3,7 @@ import { unstable_getServerSession as getServerSession } from 'next-auth'
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 
-import { authOptions as nextAuthOptions } from '@/pages/api/auth/[...nextAuth]'
+import { authOptions as nextAuthOptions } from '@/pages/api/auth/[...nextauth]'
 import { prisma } from '@/server/db/client'
 
 export const createContext = async (
