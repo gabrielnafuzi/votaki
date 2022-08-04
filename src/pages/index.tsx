cb960352abd69d5c024397a71d6d942b83a84426
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-08-03 23:53:48 -0300

    update ci install command

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { tw } from 'twind'
+
 import { trpc } from '@/utils/trpc'
 
 const HomePage = () => {
   const hello = trpc.useQuery(['example.getAll'])
 
   return (
-    <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+    <div
+      className={tw`pt-6 text-2xl text-blue-500 flex justify-center items-center w-full`}
+    >
       {hello.data ? <p>{hello.data[0]?.question}</p> : <p>Loading..</p>}
     </div>
   )
