d23a63208c5bc5d4e0ed1d7e527492b08e122116
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-09-05 02:12:25 -0300

    add tests to FormErrorMessage component

---
+++ src/components/form-error-message/form-error-message.spec.tsx
@@ -0,0 +1,13 @@
+import { expect, it, describe } from 'vitest'
+
+import { render, screen } from '@/utils/test-utils'
+
+import { FormErrorMessage } from './form-error-message'
+
+describe('<FormErrorMessage />', () => {
+  it('renders children correctly', () => {
+    render(<FormErrorMessage>Some error message</FormErrorMessage>)
+
+    expect(screen.getByText(/some error message/i)).toBeInTheDocument()
+  })
+})
